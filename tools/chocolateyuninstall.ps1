037732af92b6770ac00650c571a7a07b4499e4c9
Jim Gibbs <jamesvgibbs@gmail.com>
2019-03-08 15:37:38 -0500

    Change location for dashlane-chrome

---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,9 @@
+﻿$bits = Get-ProcessorBits
+$packageName = 'dashlane-chrome'
+$extensionID = 'fdjamakpfbbddfjaooikfcpapjohcfmg'
+
+if ($bits -eq 64) {
+    Remove-Item "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue
+   }else{
+    Remove-Item "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue
+}
